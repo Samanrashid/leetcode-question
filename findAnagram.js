832069ed88d161f8e855d4d9e7d3153ea447d697
Samanrashid <60883753+Samanrashid@users.noreply.github.com>
2026-02-19 09:50:16 +0530

    Add console logs for anagram check results

--- findAnagram.js
+++ findAnagram.js
@@ -3,5 +3,5 @@ function checkAnagram(str1, str2) {
   let s2 = str2.toLowerCase().replace(/\s/g, "").split("").sort().join("");
   return s1 === s2;
 }
-console.log(checkAnagram("silent", "listen"));
-console.log(checkAnagram("rat", "car"));
+console.log(checkAnagram("silent", "listen")); //true
+console.log(checkAnagram("rat", "car")); //false
